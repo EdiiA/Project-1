314cdeb89e56725fb6493d40fc686326f15c9a5e
elez-kadrijaj <ek67709@ubt-uni.net>
2023-12-09 23:25:48 +0100

    UPDATE

--- funksioniLoginpage.js
+++ funksioniLoginpage.js
@@ -1,29 +1,13 @@
-let signupBtn = document.getElementById("signupBtn");
-let signinBtn = document.getElementById("signinBtn");
-let nameField = document.getElementById("nameField");
-let title = document.getElementById("title");
 
-signinBtn.onclick = function () {
-    nameField.style.maxHeight = "0";
-    title.innerHTML = "Sign In";
-    signupBtn.classList.add("disable");
-    signinBtn.classList.remove("disable");
-}
-
-signupBtn.onclick = function () {
-    nameField.style.maxHeight = "60px";
-    title.innerHTML = "Sign Up";
-    signupBtn.classList.remove("disable");
-    signinBtn.classList.add("disable");
-}
 function validateForm() {
     var username = document.getElementById("username").value;
     var email = document.getElementById("email").value;
     var password = document.getElementById("password").value;
 
+    console.log("hello");
    
 
-    if (!/^[a-zA-Z0-9]+$/.test(username)) {
+    if (!((/^[a-zA-Z0-9]+$/).test(username))) {
         alert("Username can only contain letters and numbers");
         return false;
     }
@@ -32,15 +16,35 @@ function validateForm() {
         alert("Please enter your email");
         return false;
     }
-
-
-    // Check if the password meets certain criteria (e.g., minimum length)
-    if (password.length < 6) {
-        MessageChannel.p
+    if (password.length < 8) {
+        alert("The password must contain 6 characters");
         return false;
     }
 
-    return true; // Form is valid, allow submission
+    
+
+    return true;
+    
+    
+}
+function validateForm2() {
+    var username = document.getElementById("username2").value;
+    var password = document.getElementById("password2").value;
+
+   
+
+    if (!((/^[a-zA-Z0-9]+$/).test(username))) {
+        alert("Username can only contain letters and numbers");
+        return false;
+    }
+    if (password.length < 8) {
+        alert("The password must contain 6 characters");
+        return false;
+    }
+
+    
+    return true;
+    
 }
 
 
