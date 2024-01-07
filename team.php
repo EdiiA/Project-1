03c6d7dd3ece5a102bbf7e092cd91470ed5979f4
EdiiA <ea67877@ubt-uni.net>
2024-01-08 00:08:59 +0100

    Krijimi i Team
    Krijimi i file te ri: Team.

---
+++ team.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Team</title>
+    <link rel="stylesheet" href="team.css">
+</head>
+<body>
+    <?php
+        include('header.php');
+
+        echo $header1;
+    ?>
+
+    <main>
+        <div class="bg-foto">
+            <img src="Img/logo-1.png" alt="">
+        </div>
+    </main>
+
+    <?php
+        include('footer.php');
+    
+        echo $footer1;
+    ?>
+    
+</body>
+</html>
