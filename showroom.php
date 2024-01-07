a9b79ff4208d33fc46d877c443d81d48f8943e88
EdiiA <ea67877@ubt-uni.net>
2024-01-08 00:08:33 +0100

    Rensposiviteti Showroom
    Rregullimi i responsivitetit ne Showroom.

--- showroom.php
+++ showroom.php
@@ -2,10 +2,9 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=0">
-    <title>SHOWROOM</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Showroom</title>
     <link rel="stylesheet" href="style-showroom.css">
-
 </head>
 <body>
 
@@ -15,155 +14,144 @@
         echo $header1;
     ?>
 
-    <!-- <header class="headeri">
-        <ul>
-            <li><a href="home-page.html">HOME</a></li>
-            <li><a href="showroom.html">SHOWROOM</a></li>
-            <li><a href="about-us.html">ABOUT US</a></li>
-            <li><a href="contactus.html">CONTACT US</a></li>
-        </ul>
-    </header> -->
-   
-    
     <main>
-        <div class="bg-foto">
-           <img src="Img/logo-1.png" alt="">
-        </div>
-        <div class="logot">
-            <img src="Img/lamborghini-logo.png" alt="Lamborghini" class="logo">
-            <img src="Img/2025.png" alt="Ferrari" class="logo">
-            <img src="Img/1939.png" alt="RollsRoys" class="logo">
-            <img src="Img/porsche-logo.png" alt="Porche" class="logo">
-            <img src="Img/2028.png" alt="Bently" class="logo">
-            <img src="Img/audi-logo.png" alt="Audi" class="logo">
-            <img src="Img/bmw-logo.png" alt="BMW" class="logo">
-            <img src="Img/mercedes-benz-logo.png" alt="Merceds-benz" class="logo">
-            <img src="Img/Land_Rover_logo.png" alt="Landrover" class="logo">
-            <img src="Img/volkswagen-logo.png" alt="Wolkswagen" class="logo">
-    
-        </div>
-        
-        <hr id="hr1">
-
-        <div class="p-gr">
-            <p>Our Showroom</p>
+        <div class="bgfoto">
+            <p class="bg-logo"><img src="Img/logo-1.png" alt="logo"></p>
         </div>
 
-        <div class="fotografit">
-            <div class="foto-1">
-                <img src="Img/LL.png" alt="" class="img">
-                <div class="specifikat">
-                    <img src="Img/1939.png" alt="" class="logo-foto">
+        <div class="brands">
+            <div class="logot">
+                <img src="Img/lamborghini-logo.png" alt="Lamborghini" class="logo">
+                <img src="Img/2025.png" alt="Ferrari" class="logo">
+                <img src="Img/1939.png" alt="RollsRoys" class="logo">
+                <img src="Img/porsche-logo.png" alt="Porche" class="logo">
+                <img src="Img/2028.png" alt="Bently" class="logo">
+                <img src="Img/audi-logo.png" alt="Audi" class="logo">
+                <img src="Img/bmw-logo.png" alt="BMW" class="logo">
+                <img src="Img/mercedes-benz-logo.png" alt="Merceds-benz" class="logo">
+                <img src="Img/volkswagen-logo.png" alt="Wolkswagen" class="logo">
+            </div>
+
+            <p class="brands-title"><b>OUR CAR BRANDS</b></p>
+        </div>
+
+        <div class="collection">
+            <div class="car">
+                <p class="car-img"><img src="Img/LL.png" alt=""></p>
+                <div class="car-title">
+                    <p class="logo-foto"><img src="Img/1939.png" alt=""></p>
                     <p>ROLLS ROYCE GHOST</p>
                 </div>
                 <hr id="hr2">
-                <div class="te-dhenat">
+                <div class="car-text">
                     <p>2023</p>
                     <p>0 KM</p>
                 </div>
             </div>
-            <div class="foto-1">
-                <img src="Img/PORCHE.png" alt="" class="img">
-                <div class="specifikat">
-                    <img src="Img/porsche-logo.png" alt="" class="logo-foto">
-                    <p>Porche 911</p>
+
+            <div class="car">
+                <p class="car-img"><img src="Img/LL.png" alt=""></p>
+                <div class="car-title">
+                    <p class="logo-foto"><img src="Img/1939.png" alt=""></p>
+                    <p>ROLLS ROYCE GHOST</p>
                 </div>
                 <hr id="hr2">
-                <div class="te-dhenat">
+                <div class="car-text">
                     <p>2023</p>
                     <p>0 KM</p>
                 </div>
             </div>
-            <div class="foto-1">
-                <img src="Img/BMW.png" alt="" class="img">
-                <div class="specifikat">
-                    <img src="Img/bmw-logo.png" alt="" class="logo-foto">
-                    <p>BMW 7</p>
+
+            <div class="car">
+                <p class="car-img"><img src="Img/LL.png" alt=""></p>
+                <div class="car-title">
+                    <p class="logo-foto"><img src="Img/1939.png" alt=""></p>
+                    <p>ROLLS ROYCE GHOST</p>
                 </div>
                 <hr id="hr2">
-                <div class="te-dhenat">
+                <div class="car-text">
                     <p>2023</p>
                     <p>0 KM</p>
                 </div>
             </div>
-            <div class="foto-1">
-                <img src="Img/Q8.png" alt="" class="img">
-                <div class="specifikat">
-                    <img src="Img/audi-logo.png" alt="" class="logo-foto">
-                    <p>AUDI Q8</p>
+
+            <div class="car">
+                <p class="car-img"><img src="Img/LL.png" alt=""></p>
+                <div class="car-title">
+                    <p class="logo-foto"><img src="Img/1939.png" alt=""></p>
+                    <p>ROLLS ROYCE GHOST</p>
                 </div>
                 <hr id="hr2">
-                <div class="te-dhenat">
+                <div class="car-text">
                     <p>2023</p>
                     <p>0 KM</p>
                 </div>
             </div>
-            <div class="foto-1">
-                <img src="Img/SM.png" alt="" class="img">
-                <div class="specifikat">
-                    <img src="Img/mercedes-benz-logo.png" alt="" class="logo-foto">
-                    <p>Merceds-benz MAYBACH</p>
+
+            <div class="car">
+                <p class="car-img"><img src="Img/LL.png" alt=""></p>
+                <div class="car-title">
+                    <p class="logo-foto"><img src="Img/1939.png" alt=""></p>
+                    <p>ROLLS ROYCE GHOST</p>
                 </div>
                 <hr id="hr2">
-                <div class="te-dhenat">
+                <div class="car-text">
                     <p>2023</p>
                     <p>0 KM</p>
                 </div>
             </div>
-            <div class="foto-1">
-                <img src="Img/LAMBO1.png" alt="" class="img">
-                <div class="specifikat">
-                    <img src="Img/lamborghini-logo.png" alt="" class="logo-foto">
-                    <p>LAMBORGHINI</p>
+
+            <div class="car">
+                <p class="car-img"><img src="Img/LL.png" alt=""></p>
+                <div class="car-title">
+                    <p class="logo-foto"><img src="Img/1939.png" alt=""></p>
+                    <p>ROLLS ROYCE GHOST</p>
                 </div>
                 <hr id="hr2">
-                <div class="te-dhenat">
+                <div class="car-text">
                     <p>2023</p>
                     <p>0 KM</p>
                 </div>
             </div>
-            <div class="foto-1">
-                <img src="Img/LAMBO.png" alt="" class="img">
-                <div class="specifikat">
-                    <img src="Img/lamborghini-logo.png" alt="" class="logo-foto">
-                    <p>LAMBORGHINI</p>
+
+            <div class="car">
+                <p class="car-img"><img src="Img/LL.png" alt=""></p>
+                <div class="car-title">
+                    <p class="logo-foto"><img src="Img/1939.png" alt=""></p>
+                    <p>ROLLS ROYCE GHOST</p>
                 </div>
                 <hr id="hr2">
-                <div class="te-dhenat">
+                <div class="car-text">
                     <p>2023</p>
                     <p>0 KM</p>
                 </div>
             </div>
-            <div class="foto-1">
-                <img src="Img/BENTLY.png" alt="" class="img">
-                <div class="specifikat">
-                    <img src="Img/2028.png" alt="" class="logo-foto">
-                    <p>BENTLY</p>
+
+            <div class="car">
+                <p class="car-img"><img src="Img/LL.png" alt=""></p>
+                <div class="car-title">
+                    <p class="logo-foto"><img src="Img/1939.png" alt=""></p>
+                    <p>ROLLS ROYCE GHOST</p>
                 </div>
                 <hr id="hr2">
-                <div class="te-dhenat">
+                <div class="car-text">
                     <p>2023</p>
                     <p>0 KM</p>
                 </div>
             </div>
-            <div class="foto-1">
-                <img src="Img/RR.png" alt="" class="img">
-                <div class="specifikat">
-                    <img src="Img/Land_Rover_logo.png" alt="" class="logo-foto">
-                    <p>RANGE ROVER</p>
+
+            <div class="car">
+                <p class="car-img"><img src="Img/LL.png" alt=""></p>
+                <div class="car-title">
+                    <p class="logo-foto"><img src="Img/1939.png" alt=""></p>
+                    <p>ROLLS ROYCE GHOST</p>
                 </div>
                 <hr id="hr2">
-                <div class="te-dhenat">
+                <div class="car-text">
                     <p>2023</p>
                     <p>0 KM</p>
                 </div>
             </div>
-        </div>
-        <div class="butonat">
-            <button class="btn">1</button>
-            <button class="btn">2</button>
-            <button class="btn">3</button>
-            <button class="btn">4</button>
         </div>
     </main>
 
@@ -172,37 +160,6 @@
     
         echo $footer1;
     ?>
-
-    <!-- <footer>
-        <div class="footeri">
-            <div class="f-p1">
-                <h2>SHOWROOM</h2>
-                <ul>
-                    <li>Magj. Prishtine-Ferizaj</li>
-                    <li>Çagllavicë</li>
-                    <li>Kosovë</li>
-                </ul>
-            </div>
-            <div class="f-p2">
-                <h2>Our Links</h2>
-
-                <div>
-                    <p><a id="links" href="showroom.html">SHOWROOM</a></p>
-                    <p><a id="links" href="about-us.html">ABOUT US</a></p>
-                    <p><a id="links" href="contactus.html">CONTACT US</a></p>
-                </div>
-            </div>
-            <div class="f-fotot">
-                <h2>Social Media</h2>
-
-                <div class="social-logo">
-                    <a href="#"><img src="Img/Instagram.png" alt="" class="img-1"></a>
-                    <a href="#"><img src="Img/facebook.png" alt="" class="img-f"></a>
-                    <a href="#"><img src="Img/Tiktok.png.webp" alt="" class="img-f"></a>
-                </div>
-            </div>
-        </div>
-    </footer> -->
     
 </body>
 </html>
