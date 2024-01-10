8150cfd120f201382d162c35fe07f21da26d24c8
EdiiA <ea67877@ubt-uni.net>
2024-01-10 17:38:57 +0100

    Responsiviteti i Team
    Fillimi i responsivitetit te Team file.

--- team.php
+++ team.php
@@ -17,6 +17,47 @@
         <div class="bg-foto">
             <img src="Img/logo-1.png" alt="">
         </div>
+
+        <div class="title_div">
+            <p class="title">Team</p>
+
+            <hr id="hr1">
+
+        </div>
+
+        <div class="team">
+            <div class="profile">
+                <div class="team-img"><img src="" alt=""></div>
+
+                <div class="team-txt">
+                    <p id="team-title">Filan Fisteku</p>
+
+                    <p id="team-text">Exercitation ut ipsum nulla laborum consectetur nisi cillum. Aute ipsum ex ipsum consequat. Magna velit Lorem ad laboris nisi non laborum.</p>
+                </div>
+            </div>
+
+            <div class="profile">
+                
+                <div class="team-txt">
+                    <p id="team-title">Filan Fisteku</p>
+
+                    <p id="team-text">Exercitation ut ipsum nulla laborum consectetur nisi cillum. Aute ipsum ex ipsum consequat. Magna velit Lorem ad laboris nisi non laborum.</p>
+                </div>
+
+                <div class="team-img"><img src="" alt=""></div>
+
+            </div>
+
+            <div class="profile">
+                <div class="team-img"><img src="" alt=""></div>
+
+                <div class="team-txt">
+                    <p id="team-title">Filan Fisteku</p>
+
+                    <p id="team-text">Exercitation ut ipsum nulla laborum consectetur nisi cillum. Aute ipsum ex ipsum consequat. Magna velit Lorem ad laboris nisi non laborum.</p>
+                </div>
+            </div>
+        </div>
     </main>
 
     <?php
