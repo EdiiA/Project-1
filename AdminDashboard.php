ca489604c6a2b345073c6f8a76f770339e5b6071
elez-kadrijaj <ek67709@ubt-uni.net>
2024-01-21 21:52:31 +0100

    Krijimi i Faqes AdminDashboard

---
+++ AdminDashboard.php
@@ -0,0 +1,38 @@
+   
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="dashboard.css">
+</head>
+<body>
+<?php
+        include('header.php');
+
+        echo $header1;
+    ?>
+
+   <h1>Dashboards</h1>
+   
+   <nav>
+    <ul>
+      <li><a href="#">Produkte</a></li>
+      <li><a href="#">Porositë</a></li>
+      <li><a href="#">Statistikat</a></li>
+      <!-- Shtoni opsione shtesë sipas nevojave tuaja -->
+    </ul>
+  </nav>
+
+
+
+
+
+<?php
+        include('footer.php');
+    
+        echo $footer1;
+    ?>
+</body>
+</html>
