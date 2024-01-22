a2d089bf828f560060aee6538c5644580437cde0
EdiiA <ea67877@ubt-uni.net>
2024-01-22 02:15:11 +0100

    Register design
    Rregullimi i design te Register file dhe php.

--- register.php
+++ register.php
@@ -1,6 +1,5 @@
-Register: <?php
-    // 
-    
+<?php
+    include_once 'Vetura.php';
     include_once 'VeturaRepository.php';
 
     if (isset($_POST['submitbtn'])) {
@@ -21,20 +20,57 @@ Register: <?php
 
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Register</title>
-</head>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Register</title>
+        <link rel="stylesheet" href="register.css">
+    </head>
 <body>
-    <form action="<?php echo $SERVER['PHP_SELF']?>" method="post">  
-        <input type="text" name="emri" ><br>
-        <input type="text" name="vitiProdhimit" ><br>
-        <input type="text" name="km" ><br>
-        <input type="email" name="motori" ><br>
-        <input type="text" name="hp" ><br>
-        <input type="text" name="cmimi" ><br>
-        <input type="submit" name="submitbtn" value="Submit">
-    </form>
+    <?php
+        include('header.php');
+
+        echo $header1;
+    ?>
+
+    <div class="register-title">
+        <p>Register</p>
+    </div>
+
+    <div class="form-div">
+        <form class="register-form" action="<?php echo $SERVER['PHP_SELF']?>" method="post">
+            <div>
+                <label for="">Emri:</label>  
+                <input class="input-field-1" type="text" name="emri" >
+
+                <label for="">Viti i Prodhimit:</label>
+                <input class="input-field-2" type="text" name="vitiProdhimit" >
+            </div><br>
+
+            <div>
+                <label for="">Km:</label>
+                <input class="input-field" type="text" name="km" >
+
+                <label for="">Motori:</label>
+                <input class="input-field" type="email" name="motori" >
+            </div><br>
+
+            <div>
+                <label for="">HP:</label>
+                <input class="input-field" type="text" name="hp" >
+
+                <label for="">Cmimi:</label>
+                <input class="input-field" type="text" name="cmimi" >
+            </div><br>
+            
+            <input class="register-submit" type="submit" name="submitbtn" value="Submit" href="AdminDashboard.php">
+        </form>
+    </div>
+
+    <?php
+        include('footer.php');
+    
+        echo $footer1;
+    ?>
 </body>
 </html>
