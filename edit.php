e886e6c96cc4ad13e1a29dbfb759987688a79fe2
EdiiA <ea67877@ubt-uni.net>
2024-01-24 13:21:29 +0100

    Edit responsivitet
    Perfundimi i responsivitetit ne Edit file.

--- edit.php
+++ edit.php
@@ -1,18 +1,30 @@
 <?php
-include 'VeturaRepository.php';
-$id = $_GET['id'];//e merr id e studentit prej url
+// include 'VeturaRepository.php';
+// $id = $_GET['id'];//e merr id e studentit prej url
 
-$strep = new VeturaRepository();
-$student = $strep->getVeturaById($id);
+// $strep = new VeturaRepository();
+// $student = $strep->getVeturaById($id);
 ?>
 
 <!DOCTYPE html>
 <html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Edit</title>
+    <link rel="stylesheet" href="edit.css">
+</head>
 <body>
-    <h3>Edit Vetura</h3>
+    <?php
+        include('header.php');
+        
+        echo $header1;
+    ?>
+
+    <!-- <h3>Edit Vetura</h3>
     <form action="<?php echo $SERVER['PHP_SELF']?>" method="POST">
-     <!-- nese nuk duam t'i ndryshojme te gjitha te dhenat, e perdorim kete pjesen tek value qe te na shfaqen vlerat aktuale, ashtu qe atributet qe nuk duam t'i ndryshojme mbesin te njejta pa pasur nevoje t'i shkruajme prape-->
-        <input type="text" name="emri"  value="<?php echo $vetura['Emri']?>"> <br> <br> <!-- Pjesa brenda [] eshte emri i sakte i atributit si ne Databaze-->
+    
+        <input type="text" name="emri"  value="<?php echo $vetura['Emri']?>"> <br> <br> 
         <input type="text" name="vitiProdhimit"  value="<?php echo $vetura['VitiProdhimit']?>"> <br> <br>
         <input type="text" name="km"  value="<?php echo $vetura['Km']?>"> <br> <br>
         <input type="date" name="motori"  value="<?php echo $vetura['Motori']?>"> <br> <br>
@@ -20,7 +32,7 @@ $student = $strep->getVeturaById($id);
         <input type="text" name="cmimi"  value="<?php echo $vetura['Cmimi']?>"> <br> <br>
         
         <input type="submit" name="editBtn" value="save"> <br> <br>
-    </form>
+    </form> -->
 
 
 
@@ -30,49 +42,56 @@ $student = $strep->getVeturaById($id);
 
     <div class="form-div">
         <form class="edit-form" action="<?php echo $SERVER['PHP_SELF']?>" method="post">
+             <!-- nese nuk duam t'i ndryshojme te gjitha te dhenat, e perdorim kete pjesen tek value qe te na shfaqen vlerat aktuale, ashtu qe atributet qe nuk duam t'i ndryshojme mbesin te njejta pa pasur nevoje t'i shkruajme prape-->
             <div>
                 <label for="">Emri:</label>  
-                <input type="text" name="emri"  value="<?php echo $vetura['Emri']?>">
+                <input class="input-field-1" type="text" name="emri"  value="<?php echo $vetura['Emri']?>"><!-- Pjesa brenda [] eshte emri i sakte i atributit si ne Databaze-->
 
                 <label for="">Viti i Prodhimit:</label>
-                <input type="text" name="vitiProdhimit"  value="<?php echo $vetura['VitiProdhimit']?>">
+                <input class="input-field-2" type="text" name="vitiProdhimit"  value="<?php echo $vetura['VitiProdhimit']?>">
             </div><br>
 
             <div>
                 <label for="">Km:</label>
-                <input type="text" name="km"  value="<?php echo $vetura['Km']?>">
+                <input  class="input-field" type="text" name="km"  value="<?php echo $vetura['Km']?>">
 
                 <label for="">Motori:</label>
-                <input type="date" name="motori"  value="<?php echo $vetura['Motori']?>">
+                <input  class="input-field" type="date" name="motori"  value="<?php echo $vetura['Motori']?>">
             </div><br>
 
             <div>
                 <label for="">HP:</label>
-                <input type="text" name="hp"  value="<?php echo $vetura['Hp']?>">
+                <input  class="input-field" type="text" name="hp"  value="<?php echo $vetura['Hp']?>">
 
                 <label for="">Cmimi:</label>
-                <input type="text" name="cmimi"  value="<?php echo $vetura['Cmimi']?>">
+                <input  class="input-field" type="text" name="cmimi"  value="<?php echo $vetura['Cmimi']?>">
             </div><br>
             
-            <input class="edit-submit" type="submit" name="editBtn" value="save" href="AdminDashboard.php">
+            <input class="edit-submit" type="submit" name="editBtn" value="Save Changes" href="AdminDashboard.php">
         </form>
     </div>
+
+    <?php
+        include('footer.php');
+    
+        echo $footer1;
+    ?>
 </body>
 </html>
 
 <?php 
 
-if(isset($_POST['editBtn'])){
-    $emri = $_POST['emri']; //merret nga formulari
-    $vitiProdhimit = $_POST['$vitiProdhimit'];
-    $km = $_POST['km'];
-    $motori = $_POST['motori'];
-    $hp = $_POST['hp'];
-    $cmimi = $_POST['cmimi'];
+// if(isset($_POST['editBtn'])){
+//     $emri = $_POST['emri']; //merret nga formulari
+//     $vitiProdhimit = $_POST['$vitiProdhimit'];
+//     $km = $_POST['km'];
+//     $motori = $_POST['motori'];
+//     $hp = $_POST['hp'];
+//     $cmimi = $_POST['cmimi'];
     
 
-    $strep->editVetura($id,$emri,$vitiProdhimit,$km,$motori,$hp,$cmimi);
-    header("location:AdminDashboard.php");
-}
+//     $strep->editVetura($id,$emri,$vitiProdhimit,$km,$motori,$hp,$cmimi);
+//     header("location:AdminDashboard.php");
+// }
 
 ?>
