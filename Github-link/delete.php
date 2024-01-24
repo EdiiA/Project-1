756db096a9343b9c7d9020c5d13358731e424137
EdiiA <ea67877@ubt-uni.net>
2024-01-24 10:58:05 +0100

    Ndrrimi i file location

--- Github-link/delete.php
+++
@@ -1,9 +0,0 @@
-<?php
-include_once 'VeturaRepository.php';
-$id = $_GET['ID'];//e merr id prej url
-
-$strep = new VeturaRepository();
-$strep->deleteVetura($id);
-
-header("location:AdminDashboard.php");
-?>
