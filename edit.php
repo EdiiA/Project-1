d4dad4107b57aabc2080455ffe194c784a800d78
EdiiA <ea67877@ubt-uni.net>
2024-01-24 22:50:31 +0100

    Edit/Delete
    Perfundimi i edit dhe delete

--- edit.php
+++ edit.php
@@ -1,9 +1,9 @@
 <?php
-// include 'VeturaRepository.php';
-// $id = $_GET['id'];//e merr id e studentit prej url
+include 'VeturaRepository.php';
+$id = $_GET['id'];//e merr id e studentit prej url
 
-// $strep = new VeturaRepository();
-// $student = $strep->getVeturaById($id);
+$strep = new VeturaRepository();
+$vetura = $strep->getVeturaById($id);
 ?>
 
 <!DOCTYPE html>
@@ -41,7 +41,7 @@
     </div>
 
     <div class="form-div">
-        <form class="edit-form" action="<?php echo $SERVER['PHP_SELF']?>" method="post">
+        <form class="edit-form" action="<?php echo $SERVER['PHP_SELF']?>" method="POST">
              <!-- nese nuk duam t'i ndryshojme te gjitha te dhenat, e perdorim kete pjesen tek value qe te na shfaqen vlerat aktuale, ashtu qe atributet qe nuk duam t'i ndryshojme mbesin te njejta pa pasur nevoje t'i shkruajme prape-->
             <div>
                 <label for="">Emri:</label>  
@@ -56,18 +56,20 @@
                 <input  class="input-field" type="text" name="km"  value="<?php echo $vetura['Km']?>">
 
                 <label for="">Motori:</label>
-                <input  class="input-field" type="date" name="motori"  value="<?php echo $vetura['Motori']?>">
+                <input  class="input-field" type="text" name="motori"  value="<?php echo $vetura['Motori']?>">
             </div><br>
 
             <div>
                 <label for="">HP:</label>
-                <input  class="input-field" type="text" name="hp"  value="<?php echo $vetura['Hp']?>">
+                <input  class="input-field" type="text" name="hp"  value="<?php echo $vetura['HP']?>">
 
                 <label for="">Cmimi:</label>
                 <input  class="input-field" type="text" name="cmimi"  value="<?php echo $vetura['Cmimi']?>">
             </div><br>
             
-            <input class="edit-submit" type="submit" name="editBtn" value="Save Changes" href="AdminDashboard.php">
+            <input type="submit" name="editBtn" value="save">
+
+            <!-- <button name="editBtn"><a href="AdminDashboard.php">Save Changes</a></button> -->
         </form>
     </div>
 
@@ -81,17 +83,18 @@
 
 <?php 
 
-// if(isset($_POST['editBtn'])){
-//     $emri = $_POST['emri']; //merret nga formulari
-//     $vitiProdhimit = $_POST['$vitiProdhimit'];
-//     $km = $_POST['km'];
-//     $motori = $_POST['motori'];
-//     $hp = $_POST['hp'];
-//     $cmimi = $_POST['cmimi'];
+if(isset($_POST['editBtn'])){
+    $id = $puna['ID'];
+    $emri = $_POST['emri']; //merret nga formulari
+    $vitiProdhimit = $_POST['$vitiProdhimit'];
+    $km = $_POST['km'];
+    $motori = $_POST['motori'];
+    $hp = $_POST['hp'];
+    $cmimi = $_POST['cmimi'];
     
 
-//     $strep->editVetura($id,$emri,$vitiProdhimit,$km,$motori,$hp,$cmimi);
-//     header("location:AdminDashboard.php");
-// }
+    $strep->editVetura($id,$emri,$vitiProdhimit,$km,$motori,$hp,$cmimi);
+    header("location:AdminDashboard.php");
+}
 
 ?>
