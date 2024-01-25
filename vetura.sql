494847f9ce112b59c62db345b116a8c8b73672db
EdiiA <ea67877@ubt-uni.net>
2024-01-25 22:53:11 +0100

    Update vetura.sql

--- vetura.sql
+++ vetura.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 24, 2024 at 01:21 AM
+-- Generation Time: Jan 25, 2024 at 10:51 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `grande`
+-- Database: `register`
 --
 
 -- --------------------------------------------------------
@@ -34,8 +34,17 @@ CREATE TABLE `vetura` (
   `Km` int(11) NOT NULL,
   `Motori` float NOT NULL,
   `HP` float NOT NULL,
-  `Cmimi` double NOT NULL
+  `Cmimi` double NOT NULL,
+  `Foto` blob NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `vetura`
+--
+
+INSERT INTO `vetura` (`ID`, `Emri`, `VitiProdhimit`, `Km`, `Motori`, `HP`, `Cmimi`, `Foto`) VALUES
+(3, 'Porsche 911', 2020, 85000, 3.8, 530, 250000, 0x504f524348452e706e67),
+(4, 'Audi RS Q8', 2021, 55000, 4.2, 600, 1400000, 0x51382e706e67);
 
 --
 -- Indexes for dumped tables
@@ -55,7 +64,7 @@ ALTER TABLE `vetura`
 -- AUTO_INCREMENT for table `vetura`
 --
 ALTER TABLE `vetura`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
